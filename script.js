dab240a9080390b44bde94e02c7d63e7ec37210d
tiagolottficorrea-svg <tiagolottficorrea@gmail.com>
2026-08-03 17:44:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const festa = new Date("August 8, 2026 20:00:00").getTime();
 
+
+setInterval(function(){
+
+
+let agora = new Date().getTime();
+
+
+let distancia = festa - agora;
+
+
+
+let dias = Math.floor(distancia/(1000*60*60*24));
+
+let horas = Math.floor(
+(distancia%(1000*60*60*24))/(1000*60*60)
+);
+
+
+let minutos = Math.floor(
+(distancia%(1000*60*60))/(1000*60)
+);
+
+
+let segundos = Math.floor(
+(distancia%(1000*60))/1000
+);
+
+
+
+document.getElementById("days").innerHTML=dias;
+
+document.getElementById("hours").innerHTML=horas;
+
+document.getElementById("minutes").innerHTML=minutos;
+
+document.getElementById("seconds").innerHTML=segundos;
+
+
+
+},1000);
